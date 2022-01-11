bb80b3bcf4aed383a0dfae1ba8e010237545763d
HP <HP@HP-PC>
2022-01-11 23:52:51 +0530

    2nd message 11/1/2022

---
+++ practiceproject/src/mainproject/addtion.java
@@ -0,0 +1,24 @@
+package mainproject;
+
+
+
+public class addtion {
+	public static void main(StringBuilder[] args) {
+		
+		
+		
+		for(int i=1; i<=5; i++) {
+			
+		 for(int j=5; j<=i;j++) 
+		 
+		System.out.print(" * ")	; 
+		 
+		System.out.println(" ");
+		
+		 }
+		
+		
+		 
+	}
+	}
+
